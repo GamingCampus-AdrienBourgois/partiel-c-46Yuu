5892784141b1e548c59de740cf0872dda0fdd041
46Yuu <htran@gaming.tech>
2024-01-19 13:23:41 +0100

    exo 1 essai 1

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -1,4 +1,5 @@
 #include "Solution1.h"
+#include <cmath>
 
 // Don't fortget to enable the exercise in the SudentConfiguration.h file !
 #include "StudentConfiguration.h"
@@ -6,7 +7,33 @@
 
 float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, TemperatureUnits _to)
 {
-	return -1.0f;
+    float result;
+
+    if (_from == TemperatureUnits::CELSIUS) {
+        if (_to == TemperatureUnits::FAHRENHEIT) {
+            result = (9.0f / 5.0f) * _value + 32.0f;
+        }
+        else if (_to == TemperatureUnits::KELVIN) {
+            result = _value + 273.0f;
+        }
+    }
+    else if (_from == TemperatureUnits::KELVIN) {
+        if (_to == TemperatureUnits::FAHRENHEIT) {
+            result = (9.0f / 5.0f) * (_value - 273.0f) + 32.0f;
+        }
+        else if (_to == TemperatureUnits::CELSIUS) {
+            result = _value - 273.0f;
+        }
+    }
+    else if (_from == TemperatureUnits::FAHRENHEIT) {
+        if (_to == TemperatureUnits::CELSIUS) {
+            result = (5.0f / 9.0f) * (_value - 32.0f);
+        }
+        else if (_to == TemperatureUnits::KELVIN) {
+            result = (5.0f / 9.0f) * (_value - 32.0f) + 273.0f;
+        }
+    }
+    return roundf(result * 100) / 100;
 }
 
 #endif

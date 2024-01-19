7a8a17b5176a3160962b5e219bb2dffc8adb84b9
46Yuu <htran@gaming.tech>
2024-01-19 13:33:40 +0100

    exo 1 essai 2

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -1,5 +1,6 @@
 #include "Solution1.h"
 #include <cmath>
+#include <iostream>
 
 // Don't fortget to enable the exercise in the SudentConfiguration.h file !
 #include "StudentConfiguration.h"
@@ -7,22 +8,22 @@
 
 float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, TemperatureUnits _to)
 {
-    float result;
+    float result = _value;
 
     if (_from == TemperatureUnits::CELSIUS) {
         if (_to == TemperatureUnits::FAHRENHEIT) {
             result = (9.0f / 5.0f) * _value + 32.0f;
         }
         else if (_to == TemperatureUnits::KELVIN) {
-            result = _value + 273.0f;
+            result = _value + 273.15f;
         }
     }
     else if (_from == TemperatureUnits::KELVIN) {
         if (_to == TemperatureUnits::FAHRENHEIT) {
-            result = (9.0f / 5.0f) * (_value - 273.0f) + 32.0f;
+            result = (9.0f / 5.0f) * (_value - 273.15f) + 32.0f;
         }
         else if (_to == TemperatureUnits::CELSIUS) {
-            result = _value - 273.0f;
+            result = _value - 273.15f;
         }
     }
     else if (_from == TemperatureUnits::FAHRENHEIT) {
@@ -30,10 +31,10 @@ float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, Temper
             result = (5.0f / 9.0f) * (_value - 32.0f);
         }
         else if (_to == TemperatureUnits::KELVIN) {
-            result = (5.0f / 9.0f) * (_value - 32.0f) + 273.0f;
+            result = (5.0f / 9.0f) * (_value - 32.0f) + 273.15f;
         }
     }
-    return roundf(result * 100) / 100;
+    return (result * 100.0f) / 100.0f;
 }
 
 #endif
